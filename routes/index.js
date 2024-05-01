6335e019bbe53e2e7d338a0512b688fbc9b3465b
SOMEONE1703 <siphelel.1703@gmail.com>
2024-05-01 13:29:34 +0200

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+//handle game setup 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const filePath = path.join(__dirname, "../public/index.html");
+  res.sendFile(filePath);
 });
 
 module.exports = router;
