da17ef335777175d0297b13881901968948c877b
SOMEONE1703 <siphelel.1703@gmail.com>
2024-05-01 16:35:19 +0200

    anything

---
+++ public/javascripts/play.js
@@ -0,0 +1,18 @@
+console.log("starting");
+
+// Establish a connection to the server
+const socket = io("/game");
+
+socket.on("identity", (identity) => {
+    console.log("Received identity:", identity);
+    
+});
+
+
+socket.on("connect_error", (error) => {
+    console.error("Connection error:", error);
+});
+
+socket.on("error", (error) => {
+    console.error("Socket error:", error);
+});
