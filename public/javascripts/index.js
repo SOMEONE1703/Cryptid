de1a62f1992c241de70368b0755eeb038a50ee66
SOMEONE1703 <siphelel.1703@gmail.com>
2024-05-03 15:09:58 +0200

    message

---
+++ public/javascripts/index.js
@@ -0,0 +1,8 @@
+let log=document.getElementById("Login");
+log.addEventListener("click",()=>{
+    window.location.href="/users/login"
+});
+let sign=document.getElementById("Login");
+sign.addEventListener("click",()=>{
+    window.location.href="/users/register"
+});
