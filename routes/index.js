2c93ee9e9faf0c13fd8ea0f51c60474afc33f469
Massamba505 <sabelomaphalala131@gmail.com>
2024-05-09 23:08:42 +0200

    routing map json

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-
+const fs = require('fs');
 
 //handle game setup 
 router.get('/', function(req, res, next) {
@@ -17,6 +17,25 @@ router.get('/home', function(req, res, next) {
 router.get('/custom-room', function(req, res, next) {
   const filePath = path.join(__dirname, "../public/game_room.html");
   res.sendFile(filePath);
+});
+
+router.get('/maps/intro', function(req, res, next) {
+  const directoryPath = path.join(__dirname, '../public/maps/intro');
+  
+  fs.readdir(directoryPath, function(err, files) {
+    if (err) {
+      return res.status(500).send('Error reading directory');
+    }
+
+    const jsonFiles = files.filter(file => file.endsWith('.json'));
+
+    if (jsonFiles.length === 0) {
+      return res.status(404).send('No JSON files found');
+    }
+    const randomJsonFile = jsonFiles[Math.floor(Math.random() * jsonFiles.length)];
+
+    res.json({randomJsonFile});
+  });
 });
 
 router.get('/game-mode', function(req, res, next) {
