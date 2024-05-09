bd5929ba81eb5f9bd6e7ffe7d4b653c9b95d72a2
Massamba <160495255+Massamba505@users.noreply.github.com>
2024-05-09 13:05:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -13,21 +12,14 @@ var gameRouter = require('./routes/game');
 
 var app = express();
 
-// var server = require('http').Server(app);
-//var io = require('socket.io')(server);
-
 app.use(session({
-  secret:"Cryptid Web App ~ by The Greatest Team!!!",
-  resave: false,
-  saveUninitialized:false,
-  cookie:{
-      maxAge: 6000 * 60
-  }
+    secret:"CryptidWebAp~byTheGreatestTeam!!!",
+    resave: false,
+    saveUninitialized:false,
+    cookie:{
+        maxAge: 6000 * 60
+    }
 }));
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -38,21 +30,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/game', gameRouter);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-//module.exports = {app,server};
-module.exports=app;
+module.exports = app;
